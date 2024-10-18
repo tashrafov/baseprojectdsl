ae2c0f384482ed5cf46980b82e462e578c4ebe05
Taghi Ashrafov <tashrafov@gmail.com>
2024-10-18 18:46:08 +0400

    Add core Cache(PrefUtils and DataStore) module

--- app/src/main/java/com/taghiashrafov/baseprojectdsl/di/AppApiModule.kt
+++ app/src/main/java/com/taghiashrafov/baseprojectdsl/di/AppApiModule.kt
@@ -1,8 +1,23 @@
 package com.taghiashrafov.baseprojectdsl.di
 
+import com.ashrafovtaghi.cache_api.DataStore
+import com.ashrafovtaghi.cache_api.PrefUtils
+import com.ashrafovtaghi.cache_impl.CacheApi
 import dagger.Module
+import dagger.Provides
+import javax.inject.Singleton
 
 @Module
 class AppApiModule{
+    @Singleton
+    @Provides
+    fun providePrefUtilsApi(cacheApi: CacheApi): PrefUtils {
+        return cacheApi.getPrefUtils()
+    }
 
+    @Singleton
+    @Provides
+    fun provideDataStoreApi(cacheApi: CacheApi): DataStore {
+        return cacheApi.getDataStore()
+    }
 }
